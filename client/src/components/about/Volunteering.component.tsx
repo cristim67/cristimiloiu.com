8fb5392b67d7b72e99e8e0668c964b919d4a90c7
Cristi Miloiu <miloiuc4@gmail.com>
2024-10-04 19:36:57 +0300

    Update website

--- client/src/components/about/Volunteering.component.tsx
+++ client/src/components/about/Volunteering.component.tsx
@@ -9,18 +9,18 @@ interface VolunteeringItem {
 
 const volunteeringContent: VolunteeringItem[] = [
   {
+    year: "March 2024 - present",
+    rol: "Student Advisor",
+    institute: "Faculty of Electronics, Telecommunications and Information Technology",
+    details: ``,
+  },
+  {
     year: "October 2023-present",
     rol: "Board Member",
     institute: "League of Electronics Students",
     details: `Representing the students of my year of study in the Board of Directors of the Electronics Students League.
 Maintaining constant liaison with the students I represent to find out in a timely manner the problems or concerns they are facing.
 Actively participating in all meetings of the Board of Directors`,
-  },
-  {
-    year: "October 2022- October 2023",
-    rol: "Student Advisor",
-    institute: "Faculty of Electronics, Telecommunications and Information Technology",
-    details: ``,
   },
 ];
 
